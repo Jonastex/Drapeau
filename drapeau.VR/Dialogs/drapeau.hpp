b6809b2d6cbedc69d1ac7115531e5e4ded7bbf05
Jonastex <30275981+Jonastex@users.noreply.github.com>
2020-08-25 01:44:17 +0200

    Add files via upload
    Upload

---
+++ drapeau.VR/Dialogs/drapeau.hpp
@@ -0,0 +1,49 @@
+/*
+Author : John lpb
+Par la propriété intélectuel de John Deep.
+Ce fichier est protégé, pour tout modification ou utilisation contacté moi.
+Discord: Jonastex#2733
+steam: https://steamcommunity.com/id/jonastex
+*/
+class drapeau
+{
+	idd = 11062020;
+	name = "drapeau";
+	author = "John le plus beau de tous";
+
+	class controlsBackground
+	{};
+
+	class controls
+	{
+		class haut: RscButton
+		{
+			idc = 152206;
+			onButtonClick = [flag1, 1] call BIS_fnc_animateFlag;
+			text = "click pour mettre le drapeaux en hauteur"; //--- ToDo: Localize;
+			x = 0.300736 * safezoneW + safezoneX;
+			y = 0.210987 * safezoneH + safezoneY;
+			w = 0.350705 * safezoneW;
+			h = 0.153007 * safezoneH;
+		};
+		class bas: RscButton
+		{
+			idc = 162206;
+			onButtonClick = [flag1, 0] call BIS_fnc_animateFlag;
+			text = "click pour mettre le drapeaux en bas"; //--- ToDo: Localize;
+			x = 0.300736 * safezoneW + safezoneX;
+			y = 0.465998 * safezoneH + safezoneY;
+			w = 0.350705 * safezoneW;
+			h = 0.153007 * safezoneH;
+		};
+		class quittea: RscButtonMenuCancel
+		{
+			idc = 172206;
+			onButtonClick = "closeDialog 0;";
+			x = 0.755059 * safezoneW + safezoneX;
+			y = 0.108982 * safezoneH + safezoneY;
+			w = 0.0398529 * safezoneW;
+			h = 0.0510023 * safezoneH;
+		};
+	};
+};
